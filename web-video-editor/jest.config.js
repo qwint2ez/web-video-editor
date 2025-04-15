a92e358aa7824d209d5e613f079007673d3a26dd
svidinskyalexandr@gmail.com <svidinskyalexandr@gmail.com>
2025-04-15 21:51:24 +0300

    feature/design

--- web-video-editor/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-  };
